c6b2c3b8a3cdc8e5736722fe29f050af0246d3e8
João Costa <costav1402@gmail.com>
2025-01-14 16:44:35 -0300

    build: setup the app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <header className="bg-black py-4 text-center text-lg">
+        CREWFARE ALL ACCESS
+      </header>
+      <div className="container mx-auto p-4">
+      </div>
+    </div>
+  );
+}
+
+export default App;
