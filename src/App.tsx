1b8bac47fe5df4359e39c1e2a7ec0dd5ef7b11e6
João Costa <costav1402@gmail.com>
2025-01-18 21:27:37 -0300

    feat: setup the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/atoms/Header/Header';
+import { Route, Routes } from 'react-router';
+import EventGeneralDataPage from './components/pages/EventGeneralDataPage/EventGeneralDataPage';
+import EventDetailsPage from './components/pages/EventDetailPage/EventDetailPage';
+import EventDatePage from './components/pages/EventDatePage/EventDatePage';
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <header className="bg-black py-4 text-center text-lg">
-        CREWFARE ALL ACCESS
-      </header>
-      <div className="container mx-auto p-4">
+    <div className="min-h-screen bg-zinc-900 text-white">
+      
+      <Header/>
+      <div className="ml-44">
+        <Routes>
+          <Route path='/' element={<EventGeneralDataPage />}/>
+          <Route path='/detail' element={<EventDetailsPage/>}/>
+          <Route path='/dates' element={<EventDatePage />}/>
+        </Routes>
       </div>
     </div>
   );
