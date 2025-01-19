d6890bc862ff4b245483a7c4265ed61e332994a2
João Costa <costav1402@gmail.com>
2025-01-18 21:25:29 -0300

    feat: add event general data template

---
+++ src/components/templates/EventGeneralDataTemplate/EventGeneralDataTemplate.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import EventForm from '../../organisms/EventForm/EventForm';
+import ChevronGroup from '../../molecules/ChevronGroup/ChevronGroup';
+import { useNavigate } from 'react-router';
+
+function EventGeneralDataTemplate() {
+  const navegate = useNavigate()
+  const foward = () => {
+    navegate('/detail')
+  }
+  return (
+      <div className="flex-1 p-6">
+        <h1 className="text-xl mb-4">Create Event</h1>
+        <EventForm />
+        <ChevronGroup foward={foward} disabledLeft/>
+    </div>
+  );
+}
+
+export default EventGeneralDataTemplate;
