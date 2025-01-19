622955e478a949960106ea69c821367cff296def
João Costa <costav1402@gmail.com>
2025-01-18 21:09:15 -0300

    feat: add form row day picker  molecule

---
+++ src/components/molecules/FormRowDayPicker/FormRowDayPicker.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { DateRange } from 'react-day-picker';
+import DatePicker from '../../atoms/DatePicker/DatePicker';
+interface FormRowDayPickerProps {
+    label: string;
+    [key: string]: string;
+  }
+type SelectedRangeType = {
+    from: Date | undefined;
+    to: Date | undefined;
+}
+type handleDateSelectType = (range: DateRange | undefined) => void 
+type DatePickerType = SelectedRangeType & handleDateSelectType
+function FormRowDayPicker({ label, ...rest }: FormRowDayPickerProps) {
+  const [showPicker, setShowPicker] = useState(false);
+  const [selectedRange, setSelectedRange] = useState<SelectedRangeType>({ from: undefined, to:  undefined });
+  const selectedRangeString = `${
+    selectedRange.from
+      ? selectedRange.from.toLocaleDateString()
+      : "DD/MM/YYYY"
+  } - ${
+    selectedRange.to ? selectedRange.to.toLocaleDateString() : "DD/MM/YYYY"
+  }`;
+  console.log(selectedRange)
+  const handleDateSelect = (range: DateRange | undefined) => {
+    setSelectedRange({
+      from: range?.from,
+      to: range?.to,
+    });
+    if(showPicker && selectedRange.from && selectedRange.to){
+      setShowPicker(false)
+    }
+  };
+  return (
+    <div className="flex items-center space-x-4">
+      <label className="w-40">{label}</label>
+      <DatePicker 
+        selectedRangeString={selectedRangeString} 
+        selectedRange={selectedRange} 
+        handleDateSelect={handleDateSelect} 
+        showPicker={showPicker} 
+        setShowPicker={setShowPicker} />
+    </div>
+  );
+}
+
+export default FormRowDayPicker;
