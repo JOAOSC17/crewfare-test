a0b5fce844e7c2f97c5d6ad396cb7c15aa701b08
João Costa <costav1402@gmail.com>
2025-01-18 21:24:45 -0300

    feat: add event detail form organism

---
+++ src/components/organisms/EventDetailForm/EventDetailForm.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import FormRow from '../../molecules/FormRow/FormRow';
+import Button from '../../atoms/Button/Button';
+
+function EventDetailsForm() {
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className=" border-extra-thin space-y-4 p-6 rounded-lg shadow-lg ">
+      <FormRow label="Link" inputType="text" placeholder="https://crewfare.com/events/event-name/" />
+      <FormRow label="Event Address" inputType="text" />
+      <FormRow label="Venue Name" inputType="text" />
+      <FormRow label="Featured Hotels Title" inputType="text" />
+      <FormRow label="Minimum Nights" inputType="number" min="1" />
+      <div className="flex justify-end">
+        <Button>Save</Button>
+      </div>
+    </form>
+  );
+}
+
+export default EventDetailsForm;
