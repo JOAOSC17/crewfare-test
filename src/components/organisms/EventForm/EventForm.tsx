b197ef70a11c69a118888014d3933a2d50d85a9a
João Costa <costav1402@gmail.com>
2025-01-18 21:25:03 -0300

    feat: add event form organism

---
+++ src/components/organisms/EventForm/EventForm.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import BannerUploader from '../../molecules/BannerUploader/BannerUploader';
+import Checkbox from '../../atoms/Checkbox/Checkbox';
+import Button from '../../atoms/Button/Button';
+import FormRow from '../../molecules/FormRow/FormRow';
+import FormRowSelect from '../../molecules/FormRowSelect/FormRowSelect';
+
+function EventForm() {
+  return (
+    <div className=" border-extra-thin p-6 rounded-lg shadow-lg">
+      <div className="space-y-6">
+        <FormRowSelect label='Event Type' options={['Public Event', 'Private Event']}/>
+        <FormRow inputType='text'label='Event Name' placeholder='Event Name' />
+        <BannerUploader />
+        <Checkbox id="overlayTitle" label="Overlay Title on Banner" />
+        <div className="flex justify-end">
+        {/* <Button>Save</Button> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EventForm;
