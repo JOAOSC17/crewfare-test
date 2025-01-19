6b09e2d9a32846685ac9f458f9ec1d634e5847e2
João Costa <costav1402@gmail.com>
2025-01-18 21:24:30 -0300

    feat: add event dates form organism

---
+++ src/components/organisms/EventDatesForm/EventDatesForm.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import FormRow from '../../molecules/FormRow/FormRow';
+import DatePicker from '../../atoms/DatePicker/DatePicker';
+import FormRowDayPicker from '../../molecules/FormRowDayPicker/FormRowDayPicker';
+import FormRowSelect from '../../molecules/FormRowSelect/FormRowSelect';
+
+function EventDatesForm () {
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className="border-extra-thin space-y-4 p-6 rounded-lg shadow-lg ">
+
+      <FormRowDayPicker label="Bookable Start & End Dates"/>
+      <FormRowDayPicker label="Event Start and End Dates"/>
+      <div className="flex items-center space-x-2">
+        <button type="button" className="text-green-400">+ Add Event Date Range</button>
+      </div>
+
+      <FormRowDayPicker label="Default Check-In & Check-Out Dates"/>
+
+      <h3 className="text-lg mt-6 mb-4">Taxes & Fees</h3>
+      <FormRow label="Name" inputType="text" />
+      <FormRow label="Amount" inputType="number" />
+      <FormRowSelect label="Type" inputType="select" options={['Fixed', 'Percentage']} />
+
+      <div className="flex items-center space-x-2">
+        <button type="button" className="text-green-400">+ Add New Tax/Fee</button>
+      </div>
+
+      <div className="flex justify-end">
+        {/* <Button>Save</Button> */}
+      </div>
+    </form>
+  );
+}
+
+export default EventDatesForm;
